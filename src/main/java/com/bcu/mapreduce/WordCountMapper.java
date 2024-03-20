b3b461c8dfa1b85f3d4c7fd8f62b5276aee2fcf9
reigadegr <2722688642@qq.com>
2024-03-20 16:13:09 +0800

    init

---
+++ src/main/java/com/bcu/mapreduce/WordCountMapper.java
@@ -0,0 +1,18 @@
+package com.bcu.mapreduce;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, LongWritable>.Context context) throws IOException, InterruptedException {
+//        super.map(key, value, context);
+        String line = value.toString();
+        String[] words = line.split(",");
+        for (String word : words) {
+            context.write(new Text(word), new LongWritable(1));
+        }
+    }
+}
