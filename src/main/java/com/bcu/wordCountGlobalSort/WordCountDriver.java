162e7ffc8e16d1bdbebd763a77f5b4ef82987978
reigadegr <2722688642@qq.com>
2024-04-03 14:54:32 +0800

    完善驱动类

--- src/main/java/com/bcu/wordCountGlobalSort/WordCountDriver.java
+++ src/main/java/com/bcu/wordCountGlobalSort/WordCountDriver.java
@@ -1,15 +1,23 @@
 package com.bcu.wordCountGlobalSort;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class WordCountDriver extends Configured implements Tool {
 
+
+    public static void main(String[] args) throws Exception {
+        int status = ToolRunner.run(new Configuration(), new WordCountDriver(), args);
+        System.exit(status);
+    }
 
     @Override
     public int run(String[] strings) throws Exception {
@@ -30,7 +38,12 @@ public class WordCountDriver extends Configured implements Tool {
 
         //reduce
         job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
 
-        return 0;
+        //output
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job, new Path("hdfs://192.168.100.101:9000/wordcount_sort"));
+        return job.waitForCompletion(true) ? 0 : -1;
     }
 }
